db814e3126f3d8690f0ede5999440f33ac9fee06
Samuel <s.torresf@uniandes.edu.co>
2021-02-10 08:46:19 -0500

    adding new and corrected results

--- run_b.sh
+++ run_b.sh
@@ -1,19 +1,25 @@
 
-# interventions and school capacity of 25%
-python run_interventions.py --population 100000 --intervention 0.6 --school_occupation 0.25
-python run_interventions.py --population 100000 --intervention 0.4 --school_occupation 0.25
-python run_interventions.py --population 100000 --intervention 0.2 --school_occupation 0.25
 # interventions and school capacity of 15%
-# python run_interventions.py --population 100000 --intervention 0.6 --school_occupation 0.15
-# python run_interventions.py --population 100000 --intervention 0.4 --school_occupation 0.15
-# python run_interventions.py --population 100000 --intervention 0.2 --school_occupation 0.15
+python run_interventions.py --population 100000 --intervention 0.6 --school_occupation 0.15
+python run_interventions.py --population 100000 --intervention 0.4 --school_occupation 0.15
+python run_interventions.py --population 100000 --intervention 0.2 --school_occupation 0.15
 
-# ## Alternancy 50% - 50%
-# python run_interventions.py --population 100000 --intervention 0.6 --school_occupation 0.5 --school_alternancy True
-# python run_interventions.py --population 100000 --intervention 0.4 --school_occupation 0.5 --school_alternancy True
-# python run_interventions.py --population 100000 --intervention 0.2 --school_occupation 0.5 --school_alternancy True
+# interventions and school capacity of 0%
+python run_interventions.py --population 100000 --intervention 0.6 --school_occupation 0.0
+python run_interventions.py --population 100000 --intervention 0.4 --school_occupation 0.0
+python run_interventions.py --population 100000 --intervention 0.2 --school_occupation 0.0
+
+# ## Alternancy 55% - 55%
+python run_interventions.py --population 100000 --intervention 0.6 --school_occupation 0.55 --school_alternancy True
+python run_interventions.py --population 100000 --intervention 0.4 --school_occupation 0.55 --school_alternancy True
+python run_interventions.py --population 100000 --intervention 0.2 --school_occupation 0.55 --school_alternancy True
+
 # ## Alternancy 35%
-# python run_interventions.py --population 100000 --intervention 0.6 --school_alternancy True
-# python run_interventions.py --population 100000 --intervention 0.4 --school_alternancy True
-# python run_interventions.py --population 100000 --intervention 0.2 --school_alternancy True
+python run_interventions.py --population 100000 --intervention 0.6 --school_alternancy True
+python run_interventions.py --population 100000 --intervention 0.4 --school_alternancy True
+python run_interventions.py --population 100000 --intervention 0.2 --school_alternancy True
 
+# ## Alternancy 15%
+python run_interventions.py --population 100000 --intervention 0.6 --school_occupation 0.15 --school_alternancy True
+python run_interventions.py --population 100000 --intervention 0.4 --school_occupation 0.15 --school_alternancy True
+python run_interventions.py --population 100000 --intervention 0.2 --school_occupation 0.15 --school_alternancy True
